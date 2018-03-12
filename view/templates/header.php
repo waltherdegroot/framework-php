93f3bd76292136fcafcb72eb9daa90ce84f8e6fb
LAPTOP-WALTHER-\Walther <waltherdegroot65@gmail.com>
2018-03-12 09:57:16 +0100

    First commit everything works possible minor bugs

--- view/templates/header.php
+++ view/templates/header.php
@@ -4,12 +4,12 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Studenten app</title>	
-	<link rel="stylesheet" href="<?= URL ?>">
+	<link rel="stylesheet" href="<?= URL ?>css/style.css">
 </head>
 <body>
 	<nav>
-	<ul>
-		<li><a href="<?= URL ?>home/index">Home</a></li>
-		<li><a href="<?= URL ?>student/index">Students</a></li>
-	</ul>
+		<ul>
+			<li><a href="<?= URL ?>home/index">Home</a></li>
+			<li><a href="<?= URL ?>calendar/index">Calendar</a></li>
+		</ul>
 	</nav>
