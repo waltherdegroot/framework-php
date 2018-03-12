277b394635ecd4d365c8bfb8d6c3c2908dd52c18
LAPTOP-WALTHER-\Walther <waltherdegroot65@gmail.com>
2018-03-12 19:57:19 +0100

    style changed

--- view/calendar/delete.php
+++ view/calendar/delete.php
@@ -16,7 +16,7 @@
 		<p class="inputName">Ik ga akkoord:</p>
 		<input id="checkBox" type="checkbox" name="checkbox" required>
 
-		<input class= "buttons" id="greenButton" type="submit" name="submit">
+		<input class= "buttons" id="greenButton" type="submit" name="submit" value="Verwijderen">
 	</form>
 	<a href="index" class= "buttons" id="redButton">Annuleren</a>
 </div>
