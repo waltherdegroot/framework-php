abb9592f06e94121de44ba95c0976335236de57a
LAPTOP-WALTHER-\Walther <waltherdegroot65@gmail.com>
2018-03-13 13:44:24 +0100

    some minor changes

--- view/home/index.php
+++ view/home/index.php
@@ -1,4 +1,12 @@
+
 <div class="container">
  <h1>Welkom bij het php-framework.</h1>
  <p>Je bent nu in home/index.</p>
 </div>
+<nav>
+	<ul>
+		<li><a href="<?= URL ?>home/index">Home</a></li>
+		<li><a href="<?= URL ?>calendar/index">Calendar</a></li>
+		<li><a href="<?= URL ?>hospital/index">Hospital</a></li>
+	</ul>
+</nav>
--- view/templates/header.php
+++ view/templates/header.php
@@ -7,9 +7,4 @@
 	<link rel="stylesheet" href="<?= URL ?>css/style.css">
 </head>
 <body>
-	<nav>
-		<ul>
-			<li><a href="<?= URL ?>home/index">Home</a></li>
-			<li><a href="<?= URL ?>calendar/index">Calendar</a></li>
-		</ul>
-	</nav>
+	
